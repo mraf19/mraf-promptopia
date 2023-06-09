c9caac998a6c2da3af0271e2975fa8adca076497
mraf19 <raflimr1902@gmail.com>
2023-06-10 06:49:24 +0700

    Initialize Project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompt",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <section className="w-full flex-center flex-col"></section>;
+}
