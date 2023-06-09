97c5996b2964dde0c1f55c69c79832378fbec074
mraf19 <raflimr1902@gmail.com>
2023-06-10 06:56:44 +0700

    making hompe page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function HomePage() {
-  return <section className="w-full flex-center flex-col"></section>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompt</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-aource AI prompting tool for modern wo0rld to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 }
